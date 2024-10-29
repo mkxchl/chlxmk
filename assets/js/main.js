3369bd542f6da5d6883197ae8c312b8a2e74b4a5
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2024-10-29 18:30:16 +0700

    Update Button Get Started

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,9 @@ const place_info = document.querySelector(".place_info");
 const hobby = document.querySelector(".hobby");
 const hobby_info = document.querySelector(".hobby_info");
 const BodyX = document.querySelector("body");
+const VxBodySocial = document.querySelector(".VxBodySocial");
+const VxBodySocialOverlay = document.querySelector(".VxBodySocialOverlay");
+const VzxHome = document.querySelector(".VzxHome");
 
 
 let BacksoundMusic = new Audio("https://editor-miring-squad.netlify.app/mix/mk-song-1.mp3");
@@ -17,4 +20,18 @@ function PlayMusic() {
     BacksoundMusic.play();
     VzxMusic.classList.add("HideMusic");
     BodyX.classList.add("ChangeTheme");
+}
+function OpenSocial() {
+    VxBodySocial.classList.add("VxBodySocialShow");
+}
+function CloseOverlay() {
+    VxBodySocial.classList.remove("VxBodySocialShow");
+}
+function VzxLoad() {
+    setTimeout(() => {
+        VxBodySocial.classList.add("VxBodySocialShow");
+    }, 2000);
+}
+function GetStarted() {
+    VzxHome.classList.add("VzxHomeShow");
 }
